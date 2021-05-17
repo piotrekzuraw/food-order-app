395dacb03156dcaab8bd43cefba5d9b6b3a89295
Piotr Zuraw <zuraw13@op.pl>
2021-05-17 14:52:00 +0200

    add HeaderCartButton component add CartIcon component to fetch svg icon style components

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import headerImg from "../../images/header-img.jpg";
 import classes from "../../styles/Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>Food App</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["header-img"]}>
         <img src={headerImg} alt="Food" />
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,16 @@
+import CartIcon from "../UI/CartIcon";
+import classes from "../../styles/HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.items}>0</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
