cd73cdd6defa0c8beaffc63949784821696b63e7
Piotr Zuraw <zuraw13@op.pl>
2021-05-17 18:16:05 +0200

    modal implementation

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,28 @@
+import classes from "../../styles/Modal.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop />, document.getElementById("overlays"))}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        document.getElementById("overlays")
+      )}
+    </>
+  );
+};
+
+export default Modal;
