6f0ec2ac86a7f185c47659830d308f4882861499
Piotr Zuraw <zuraw13@op.pl>
2021-05-17 18:09:01 +0200

    modal implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Cart from "./components/Layout/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Layout/Meals";
 
 const App = () => {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Meals />
