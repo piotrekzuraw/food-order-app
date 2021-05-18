6a4ece187a1b7c276e7ced339274ca0beb7af843
Piotr Zuraw <zuraw13@op.pl>
2021-05-18 10:56:34 +0200

    set up Ref from Input component to MealItemForm to transfer values from inputs

--- src/components/Layout/MealItemForm.js
+++ src/components/Layout/MealItemForm.js
@@ -1,10 +1,18 @@
+import { useRef } from "react";
 import Input from "../UI/Input.js";
 import classes from "../../styles/MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const inputAmountRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={handleSubmit}>
       <Input
+        ref={inputAmountRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,14 @@
+import React from "react";
 import classes from "../../styles/Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input id={props.input.id} {...props.input} />
+      <input ref={ref} id={props.input.id} {...props.input} />
       {/* Uzycie ... sprawi, że wszystkie propsy jaki zostaną przypisane komponentowi Input zostaną przypisane elementowi input powyżej */}
     </div>
   );
-};
+});
 
 export default Input;
