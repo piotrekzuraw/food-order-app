10cfb37216c51516a6ad9a491be0e59423dfc950
Piotr Zuraw <zuraw13@op.pl>
2021-05-18 11:45:18 +0200

    use context to output items and amount in cart validate visibility for order button in cart

--- src/components/Layout/Cart.js
+++ src/components/Layout/Cart.js
@@ -1,10 +1,18 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import classes from "../../styles/Cart.module.css";
 import Modal from "../UI/Modal";
 
 const Cart = (props) => {
+  const cartContext = useContext(CartContext);
+
+  const totalAmount = `PLN ${cartContext.totalAmount.toFixed(2)}`;
+
+  const hasItems = cartContext.items.length > 0;
+
   const cartItems = (
     <ul>
-      {[{ id: "m1", name: "Sushi", amount: 2, price: 39.99 }].map((item) => (
+      {cartContext.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
@@ -14,13 +22,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>39.99</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
