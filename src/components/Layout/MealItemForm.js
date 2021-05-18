293826c8065dbf5b7a86648a480ee7b49de0bbb2
Piotr Zuraw <zuraw13@op.pl>
2021-05-18 11:08:52 +0200

    add submit handler and validation for entered amount

--- src/components/Layout/MealItemForm.js
+++ src/components/Layout/MealItemForm.js
@@ -1,12 +1,25 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Input from "../UI/Input.js";
 import classes from "../../styles/MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [enteredAmountIsValid, setEnteredAmountIsValid] = useState(true);
   const inputAmountRef = useRef();
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    const enteredAmount = inputAmountRef.current.value; //zawsze dostaniemy stringa!
+    const enteredAmountNUmber = +enteredAmount;
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNUmber < 1 ||
+      enteredAmountNUmber > 5
+    ) {
+      setEnteredAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNUmber);
   };
 
   return (
@@ -24,6 +37,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>Add</button>
+      {!enteredAmountIsValid && <p>Please enter valid amount!</p>}
     </form>
   );
 };
