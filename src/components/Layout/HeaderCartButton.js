58a278c63120906e386260ea8623d149ce0deb17
Piotr Zuraw <zuraw13@op.pl>
2021-05-23 12:12:10 +0200

    add bump animation to header button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,33 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import CartIcon from "../UI/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "../../styles/HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [buttonActive, setButtonActive] = useState(false);
   const cartContext = useContext(CartContext);
 
   const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
+
+  const buttonClasses = `${classes.button} ${buttonActive ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return;
+    }
+    setButtonActive(true);
+
+    const timer = setTimeout(() => {
+      setButtonActive(false);
+    }, 250);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
